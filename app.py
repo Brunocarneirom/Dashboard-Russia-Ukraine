8671dbcb013122efed2a079b5b0075566f8ce82e
Brunocarneirom <61274147+Brunocarneirom@users.noreply.github.com>
2022-06-08 17:41:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -20,10 +20,6 @@ def get_data() -> pd.DataFrame:
 
 equipment = get_data()
 
-
-#personnel = pd.read_csv("russia_losses_personnel.csv")
-#equipment = pd.read_csv("russia_losses_equipment.csv")
-
 war = pd.merge(equipment, personnel, on=["date", "day"])
 war["equipment_loss"] = war['aircraft'] + war['helicopter'] + war['tank'] + war['APC'] + war['field artillery'] + war['naval ship']
 
@@ -54,43 +50,5 @@ with fig_col2:
     fig2 = px.line(data_frame=war, x="day", y="equipment_loss")
     st.write(fig2)
 
-def df_filter(message,war):
-
-        slider_1, slider_2 = st.slider('%s' % (message),0,len(war)-1,[0,len(war)-1],1)
-
-        while len(str(war.iloc[slider_1][1]).replace('.0','')) < 4:
-            war.iloc[slider_1,1] = '0' + str(war.iloc[slider_1][1]).replace('.0','')
-            
-        while len(str(war.iloc[slider_2][1]).replace('.0','')) < 4:
-            war.iloc[slider_2,1] = '0' + str(war.iloc[slider_1][1]).replace('.0','')
-        
-        filtered_df = war.iloc[slider_1:slider_2+1][:].reset_index(drop=True)
-
-        return filtered_df
-
-if __name__ == '__main__':
-
-    st.title('Datetime Filter')
-    filtered_df = df_filter('Move sliders to filter dataframe',war)
-
-    column_1, column_2 = st.columns(2)
-
-    with column_1:
-        st.title('APC loss')
-        st.line_chart(filtered_df['APC'])   
-
-    with column_2:
-        st.title('Aircraft loss')
-        st.line_chart(filtered_df['aircraft'])                 
-
-    column_3, column_4 = st.columns(2)      
-
-    with column_3:
-        st.title('Tank loss')
-        st.line_chart(filtered_df['tank'])
-
-    with column_4:
-        st.title('Helicopter loss')
-        st.line_chart(filtered_df['helicopter']) 
 
 ## Time frame from https://github.com/mkhorasani/interactive_datetime_filter/blob/main/interactive_datetime_filter.py       
