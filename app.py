96f731c0c5ac490a11b05596040b41b306584c55
Brunocarneirom <61274147+Brunocarneirom@users.noreply.github.com>
2022-06-08 16:53:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objs as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.figure_factory as ff
